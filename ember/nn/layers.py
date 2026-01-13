512d9a01d2d160c7b50fb8da7b3ee44ea4adfe76
pawel <paweljarosz002@gmail.com>
2026-01-14 00:04:07 +0100

    add methods to Linear layer

--- ember/nn/layers.py
+++ ember/nn/layers.py
@@ -7,19 +7,19 @@ from ember import Tensor
 
 
 class Linear(Layer):
-    def __init__(self, in_features: int, out_features: int, bias: bool = True):
+    def __init__(self, in_features: int, out_features: int):
         self.in_features = in_features
         self.out_features = out_features
-        self.bias = bias
 
-        self.x = Tensor | None
-        self.y = Tensor | None
+        self.x: Tensor | None
+        self.y: Tensor | None
 
         self.w: Tensor
-        self.b: Tensor | None
+        self.b: Tensor
 
         self.grad_w: Tensor | None
         self.grad_b: Tensor | None
+
         self.reset()
 
     def reset(self):
@@ -31,6 +31,26 @@ class Linear(Layer):
             -scale, scale, size=(self.in_features, self.out_features)
         )
 
-        # TODO: add em.zeros and em.const
-        if self.bias:
-            self.bias = em.zeros(size=())
+        self.b = em.random.zeros(size=(self.out_features,))
+
+        self.grad_w = None
+        self.grad_b = None
+
+    def parameters(self) -> list[Tensor]:
+        return [self.w, self.b]
+
+    def gradients(self) -> list[Tensor | None]:
+        return [self.grad_w, self.grad_b]
+
+    def forward(self, x: Tensor, training: bool) -> Tensor:
+        self.x = x
+        self.y = self.b + x @ self.w
+        return self.y
+
+    # TODO implement transposition and summation
+
+    # def backward(self, grad_y: Tensor) -> Tensor:
+    #     self.grad_w = self.x.T @ grad_y
+    #     self.grad_b = grad_y.sum(axis=0)
+    #     grad_x = grad_y @ self.w.T
+    #     return grad_x
